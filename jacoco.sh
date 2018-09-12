ebc69e823d5d7a9e049459b50dafbf7369b0bfeb
Cristian Kocza <>
2018-09-12 08:01:45 +0300

    jacoco

--- jacoco.sh
+++ jacoco.sh
@@ -3,24 +3,26 @@ set -euo pipefail
 
 function convert_file {
   local xccovarchive_file="$1"
-  local file_name="$2"
-  echo "  <file path=\"$file_name\">"
-  xcrun xccov view --file "$file_name" "$xccovarchive_file" | \
+  local file_path="$2"
+  filename=$(basename -- "$file_path")
+  extension="${filename##*.}"
+  filename="${filename%.*}"
+  echo "<class name=\"$filename\"><sourcefile name=\"$file_path\">"
+  xcrun xccov view --file "$file_path" "$xccovarchive_file" | \
     sed -n '
-    s/^ *\([0-9][0-9]*\): 0.*$/    <lineToCover lineNumber="\1" covered="false"\/>/p;
-    s/^ *\([0-9][0-9]*\): [1-9].*$/    <lineToCover lineNumber="\1" covered="true"\/>/p
+    s/^ *\([0-9][0-9]*\): *\([0-9][0-9]*\).*$/<line nr="\1" mi="0" ci="\2" mb="0" cb="0"\/>/p
     '
-  echo '  </file>'
+  echo '</sourcefile></class>'
 }
 
 function xccov_to_generic {
-  echo '<coverage version="1">'
+  echo '<report name="jaCoCo">'
   for xccovarchive_file in "$@"; do
     xcrun xccov view --file-list "$xccovarchive_file" | while read -r file_name; do
       convert_file "$xccovarchive_file" "$file_name"
     done
   done
-  echo '</coverage>'
+  echo '</report>'
 }
 
 xccov_to_generic build/Logs/Test/*.xccovarchive > build/reports/jacoco.xml
