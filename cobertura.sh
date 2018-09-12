0d3182e2dab5a4b78bd454ab8917a2854ea9e5ef
Cristian Kocza <>
2018-09-12 22:46:27 +0300

    cobertura

--- cobertura.sh
+++ cobertura.sh
@@ -7,7 +7,7 @@ function convert_file {
   filename=$(basename -- "$file_path")
   extension="${filename##*.}"
   basename="${filename%.*}"
-  echo "<class name=\"$basename\" filename=\"$file_path\" line-rate=\"1.0\" branch-rate=\"1.0\" complexity=\"1.0\"><lines>"
+  echo "<class name=\"$basename\" filename=\"$file_path\" line-rate=\"1.0\" branch-rate=\"1.0\" complexity=\"1.0\"><methods></methods><lines>"
   xcrun xccov view --file "$file_path" "$xccovarchive_file" | \
     sed -n '
     s/^ *\([0-9][0-9]*\): *\([0-9][0-9]*\).*$/<line number="\1" hits="\2"\/>/p
@@ -17,8 +17,8 @@ function convert_file {
 
 function xccov_to_generic {
   echo '<?xml version="1.0" ?>'
-  echo '<!DOCTYPE coverage SYSTEM "http://cobertura.sourceforge.net/xml/coverage-04.dtd">'  
-  echo '<coverage line-rate="0.9" branch-rate="0.75">'
+  echo '<!DOCTYPE coverage SYSTEM "http://cobertura.sourceforge.net/xml/coverage-04.dtd">'
+  echo '<coverage line-rate="0.0" branch-rate="0.0" lines-covered="0" lines-valid="0" branches-covered="0" branches-valid="0" complexity="0" version="4.0" timestamp="123">'
   echo '<sources><source>'`pwd`'</source></sources>'
   echo '<packages><package name="" line-rate="1.0" branch-rate="1.0" complexity="1.0"><classes>'
   for xccovarchive_file in "$@"; do
